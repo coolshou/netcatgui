0c676790bb77771ea5fbc1dd4a5164bc2f1328bf
coolshou <coolshou2060@gmail.com>
2024-02-02 13:42:09 +0800

    fix windows taskbar icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "netcatgui.h"
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/toolbar/icons/netcatgui.png"));
     NetcatGUI w;
     w.show();
     return a.exec();
